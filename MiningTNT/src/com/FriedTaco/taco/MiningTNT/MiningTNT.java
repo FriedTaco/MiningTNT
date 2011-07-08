e5be71e50c5d4e8e10f4492cea3fdc2e8a688c91
dalton.pcrepair@gmail.com <dalton.pcrepair@gmail.com>
2011-07-07 20:09:03 -0400

    Fixed TNT chaining, yield, and moved config.

--- MiningTNT/src/com/FriedTaco/taco/MiningTNT/MiningTNT.java
+++ MiningTNT/src/com/FriedTaco/taco/MiningTNT/MiningTNT.java
@@ -18,10 +18,11 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
 
+
 	public class MiningTNT extends JavaPlugin { 
 	    private final MiningTNTEntityListener entityListener = new MiningTNTEntityListener(this);
 	    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
-	    private Logger log;
+	    private Logger log = Logger.getLogger("Minecraft");
 	    public static List<String> destroy = new ArrayList<String>();
 	    double max, yield;
 	    public static boolean isConflict=false;
@@ -46,70 +47,80 @@ import org.bukkit.plugin.PluginManager;
 	        pm.registerEvent(Event.Type.ENTITY_EXPLODE, entityListener, Priority.High, this);
 	        PluginDescriptionFile pdfFile = this.getDescription();
 	        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
-	        loadSettings();
+	        try {
+				loadSettings();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	        checkForConflict();
 	    }
-	    public void loadSettings() {
-	    	final String dir = "plugins/MiningTNT";
-	        if (!new File(dir + File.separator + "MiningTNT.properties").exists()) {
-	            FileWriter writer = null;
-	            try {
-	                writer = new FileWriter(dir + File.separator + "MiningTNT.properties");
-	                writer.write("#Mining TNTn\r\n");
-	                writer.write("#Add item ID's sparated by spaces for them to be affected by TNT.\r\n");
-	                writer.write("#Example:\r\n");
-	                writer.write("#DestroyTheseBlocks=1 2 3 4 17\r\n");
-	                writer.write("DestroyTheseBlocks=\r\n");
-	                writer.write("MaxAltitude=70\r\n");
-	                writer.write("#CreeperNerf MUST be enabled if CreepersActLikeTNT is enabled.");
-	                writer.write("CreeperNerf=true\r\n");
-	                writer.write("CreepersActLikeTNT=false\r\n");
-	                writer.write("#Allow TNT to activate other TNT within the blast radius, otherwise the TNT will be destroyed.\r\n");
-	                writer.write("AllowTNTChaining=true\r\n");
-	                writer.write("#Yield is the percentage that the block is dropped rather than destroyed when in blast radius. Anything above 1 will be assumed to be 1.\r\n");
-	                writer.write("Yield=0.4\r\n");
-	                
-	                
-	                } catch (Exception e) {
-	                log.log(Level.SEVERE,
-	                        "Exception while creating MiningTNT.properties", e);
-	                try {
-	                    if (writer != null)
-	                        writer.close();
-	                } catch (IOException ex) {
-	                    log
-	                            .log(
-	                                    Level.SEVERE,
-	                                    "Exception while closing writer for MiningTNT.properties",
-	                                    ex);
-	                }
-	            } finally {
-	                try {
-	                    if (writer != null)
-	                        writer.close();
-	                } catch (IOException e) {
-	                    log
-	                            .log(
-	                                    Level.SEVERE,
-	                                    "Exception while closing writer for MiningTNT.properties",
-	                                    e);
-	                }
-	            }
-	        }
-	        
-	        PropertiesFile properties = new PropertiesFile(dir + File.separator + "MiningTNT.properties");
-	        try {
-	          destroy = Arrays.asList(properties.getString("DestroyTheseBlocks", "").split(" "));
-	          max = properties.getDouble("MaxAltitude", 70);
-	          creeper = properties.getBoolean("CreeperNerf", true);
-	          miningCreeper = properties.getBoolean("CreepersActLikeTNT", false);
-	          chain = properties.getBoolean("AllowTNTChaining", true);
-	          yield = properties.getDouble("Yield", 0.4);
-	        } catch (Exception e) {
-	            log.log(Level.SEVERE,
-	                    "Exception while reading from MiningTNT.properties", e);
-	        }
-	    }
+	    public void loadSettings() throws Exception
+		 {
+			 if (!this.getDataFolder().exists())
+			 {
+				 	this.getDataFolder().mkdirs();
+			 }
+			 final String dir = "plugins/MiningTNT";
+		        if (!new File(dir + File.separator + "MiningTNT.properties").exists()) {
+		            FileWriter writer = null;
+		            try {
+		            	writer = new FileWriter(dir + File.separator + "MiningTNT.properties");
+		                writer.write("#MiningTNT configuration\r\n");
+		                writer.write("#Add item ID's sparated by spaces for them to be affected by TNT.\r\n");
+		                writer.write("#Example:\r\n");
+		                writer.write("#DestroyTheseBlocks=1 2 3 4 17\r\n");
+		                writer.write("DestroyTheseBlocks=\r\n");
+		                writer.write("MaxAltitude=70\r\n");
+		                writer.write("#CreeperNerf MUST be enabled if CreepersActLikeTNT is enabled.");
+		                writer.write("CreeperNerf=true\r\n");
+		                writer.write("CreepersActLikeTNT=false\r\n");
+		                writer.write("#Allow TNT to activate other TNT within the blast radius, otherwise the TNT will be destroyed.\r\n");
+		                writer.write("TNT (ID 46) must be added to list of blocks to be destroyed for TNT to chain.\r\n");
+		                writer.write("AllowTNTChaining=true\r\n");
+		                writer.write("#Yield is the percentage that the block is dropped rather than destroyed when in blast radius. Anything above 1 will be assumed to be 1.\r\n");
+		                writer.write("Yield=0.4\r\n");
+		                
+		                } catch (Exception e) {
+		                log.log(Level.SEVERE,
+		                        "Exception while creating MiningTNT.properties", e);
+		                try {
+		                    if (writer != null)
+		                        writer.close();
+		                } catch (IOException ex) {
+		                    log
+		                            .log(
+		                                    Level.SEVERE,
+		                                    "Exception while closing writer for MiningTNT.properties",
+		                                    ex);
+		                }
+		            } finally {
+		                try {
+		                    if (writer != null)
+		                        writer.close();
+		                } catch (IOException e) {
+		                    log
+		                            .log(
+		                                    Level.SEVERE,
+		                                    "Exception while closing writer for MiningTNT.properties",
+		                                    e);
+		                }
+		            }
+		        }
+		        PropertiesFile properties = new PropertiesFile(dir + File.separator + "MiningTNT.properties");
+		        try {
+			          destroy = Arrays.asList(properties.getString("DestroyTheseBlocks", "").split(" "));
+			          max = properties.getDouble("MaxAltitude", 70);
+			          creeper = properties.getBoolean("CreeperNerf", true);
+			          miningCreeper = properties.getBoolean("CreepersActLikeTNT", false);
+			          chain = properties.getBoolean("AllowTNTChaining", true);
+			          yield = properties.getDouble("Yield", 0.4);
+		        } catch (Exception e) {
+		            log.log(Level.SEVERE,
+		                    "Exception while reading from MiningTNT.properties", e);
+		        }
+
+		 }
 
 	    public boolean isDebugging(final Player player) {
 	        if (debugees.containsKey(player)) {
